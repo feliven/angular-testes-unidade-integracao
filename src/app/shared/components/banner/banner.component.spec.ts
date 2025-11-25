2949e6c4dde5b09cc08b38a49970ec95068d5e77
FVS <feliven@gmail.com>
2025-11-25 15:40:59 -0300

    cria testes para banner

--- src/app/shared/components/banner/banner.component.spec.ts
+++ src/app/shared/components/banner/banner.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BannerComponent } from './banner.component';
+import { By } from '@angular/platform-browser';
 
 describe('BannerComponent', () => {
   let component: BannerComponent;
@@ -19,4 +20,20 @@ describe('BannerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve renderizar o título no template', () => {
+    const titulo = 'teste';
+
+    component.title = titulo;
+
+    fixture.detectChanges();
+
+    const tituloNaPagina = fixture.debugElement.query(
+      By.css('h1')
+    ).nativeElement;
+
+    expect(tituloNaPagina.textContent).toEqual(titulo);
+  });
+
+  // it("",()=>{})
 });
