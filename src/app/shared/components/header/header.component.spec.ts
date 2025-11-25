45217f79caa2de63c8a53285616d284c3b8c3815
FVS <feliven@gmail.com>
2025-11-25 15:41:10 -0300

    cria testes para header

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -18,5 +19,43 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve renderizar imagem no template', () => {
+    const srcImagem = 'assets/images/zoop-store.svg';
+
+    const imagemNoHeader = fixture.debugElement.query(
+      By.css('img')
+    ).nativeElement;
+
+    expect(imagemNoHeader.src).toContain(srcImagem);
+  });
+
+  it('deve renderizar nav no template', () => {
+    const navNoHeader = fixture.debugElement.query(By.css('nav')).nativeElement;
+
+    expect(navNoHeader).toBeDefined();
+  });
+
+  it('deve renderizar ul no template', () => {
+    const ulNoHeader = fixture.debugElement.query(By.css('ul')).nativeElement;
+
+    expect(ulNoHeader).toBeDefined();
+  });
+
+  it('deve renderizar 2 li no template', () => {
+    const liNoHeader = fixture.debugElement.queryAll(By.css('li'));
+
+    fixture.detectChanges();
+
+    expect(liNoHeader.length).toBe(2);
+  });
+
+  it('deve renderizar 2 links corretamente no template', () => {
+    const linksNoHeader = fixture.debugElement.queryAll(By.css('a'));
+
+    expect(linksNoHeader.length).toBe(2);
+    expect(linksNoHeader[0].nativeElement.textContent).toContain('Produtos');
+    expect(linksNoHeader[1].nativeElement.textContent).toContain('Gerenciar');
   });
 });
